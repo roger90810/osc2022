a04b56b1a6aaec533988fb7f422796a7355da4cd
Roger <nyps6516@gmail.com>
2022-03-02 17:14:46 +0800

    Move MMIO_BASE address to mmio.h.

--- src/mmio.h
+++ src/mmio.h
@@ -1,3 +1,5 @@
+#define MMIO_BASE   (0x3F000000)
+
 #define PM_PASSWORD (0x5a000000)
 #define PM_RSTC     ((volatile unsigned int*) 0x3F10001c)
 #define PM_WDOG     ((volatile unsigned int*) 0x3F100024)
