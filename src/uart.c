90a70ca6bffa6fb19494bcbfbef8ea80780378e3
Roger <nyps6516@gmail.com>
2022-03-03 18:18:10 +0800

    Fix a character show when boot.

--- src/uart.c
+++ src/uart.c
@@ -4,6 +4,16 @@
 void uart_init()
 {
     register unsigned int r;
+    /* map UART1 to GPIO pins */
+    r = *GPFSEL1;
+    r &= ~((7 << 12) | (7 << 15)); // clear GPIO14, GPIO15, each GPIO pin has 3-bit
+    r |= (2 << 12) | (2 << 15);    // set GPIO14, GPIO15 to ALT5
+    *GPFSEL1 = r;
+    *GPPUD = 0;            // enable pins 14 and 15
+    r = 150; while(r--) { asm volatile("nop"); }
+    *GPPUDCLK0 = (1 << 14) | (1 << 15);
+    r = 150; while(r--) { asm volatile("nop"); }
+    *GPPUDCLK0 = 0;        // flush GPIO setup
 
     /* initialize UART */
     *AUX_ENABLE |= 1;      // enable UART1, AUX mini uart
@@ -13,17 +23,6 @@ void uart_init()
     *AUX_MU_IER = 0;       // disable interrupt
     *AUX_MU_IIR = 0xc6;    // no FIFO
     *AUX_MU_BAUD = 270;    // set baud rate to 115200
-
-    /* map UART1 to GPIO pins */
-    r = *GPFSEL1;
-    r &= ~((7 << 12) | (7 << 15)); // clear GPIO14, GPIO15, each GPIO pin has 3-bit
-    r |= (2 << 12) | (2 << 15);    // set GPIO14, GPIO15 to ALT5
-    *GPFSEL1 = r;
-    *GPPUD = 0;            // enable pins 14 and 15
-    r = 150; while(r--) { asm volatile("nop"); }
-    *GPPUDCLK0 = (1 << 14) | (1 << 15);
-    r=150; while(r--) { asm volatile("nop"); }
-    *GPPUDCLK0 = 0;        // flush GPIO setup
     *AUX_MU_CNTL = 3;      // enable Tx, Rx
 }
 
