6cc34ac5d95b23782ab5cdb3c5058239b5626d9f
Roger <nyps6516@gmail.com>
2022-03-14 01:50:45 +0800

    Add strncmp function.

--- src/string.c
+++ src/string.c
@@ -1,6 +1,13 @@
+/**
+ * @brief Compare two strings
+ * 
+ * @param cs One string
+ * @param ct Another string
+ * @return int 
+ */
 int strcmp(const char *cs, const char *ct)
 {
-	unsigned char c1, c2;
+    unsigned char c1, c2;
 
 	while (1) {
 		c1 = *cs++;
@@ -9,6 +16,30 @@ int strcmp(const char *cs, const char *ct)
 			return c1 < c2 ? -1 : 1;
 		if (!c1)
 			break;
+	}
+	return 0;
+}
+
+/**
+ * @brief Compare two length-limited strings
+ * 
+ * @param cs One string
+ * @param ct Another string
+ * @param count The maximum number of bytes to compare
+ * @return int 
+ */
+int strncmp(const char *cs, const char *ct, unsigned int count)
+{
+	unsigned char c1, c2;
+
+	while (count) {
+		c1 = *cs++;
+		c2 = *ct++;
+		if (c1 != c2)
+			return c1 < c2 ? -1 : 1;
+		if (!c1)
+			break;
+		count--;
 	}
 	return 0;
 }
--- src/string.h
+++ src/string.h
@@ -1,1 +1,2 @@
 int strcmp(const char *, const char *);
+int strncmp(const char *, const char *, unsigned int);
