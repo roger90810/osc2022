9efbea781267f63b30251b26ad3932949cfc717a
Roger <nyps6516@gmail.com>
2022-03-14 02:05:12 +0800

    Some changes.
    Modify makefile to support debug mode.
    Modify python script to print the byte correctly.

--- src/uart_send.py
+++ src/uart_send.py
@@ -7,8 +7,9 @@ SERIAL_PATH = sys.argv[2]
 with open(KERNEL_PATH, 'rb', buffering=0) as kernel_file:
     kernel_size = os.path.getsize(KERNEL_PATH)
     print(f'size is      : {kernel_size}')
-    print(f'size in byte : {kernel_size.to_bytes(4, "big")}')
+    b = kernel_size.to_bytes(4, "big")
+    print(f'size in byte : {hex(b[0])} {hex(b[1])} {hex(b[2])} {hex(b[3])}')
     with open(SERIAL_PATH, 'wb', buffering=0) as tty:
-        tty.write(kernel_size.to_bytes(4, "big"))
+        tty.write(b)
         tty.write(kernel_file.read())
         print("Send kernel done.")
