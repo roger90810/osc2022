78ac1c81c602d31256349b439013347e357206bd
Roger <nyps6516@gmail.com>
2022-03-14 14:09:19 +0800

    Add strsep to parse arguments of cmd.

--- src/string.c
+++ src/string.c
@@ -43,3 +43,45 @@ int strncmp(const char *cs, const char *ct, unsigned int count)
 	}
 	return 0;
 }
+
+/**
+ * @brief Find the first occurrence of a set of characters
+ * 
+ * @param cs The string to be searched
+ * @param ct The characters to search for
+ * @return char* 
+ */
+char *strpbrk(const char *cs, const char *ct)
+{
+    const char *sc1, *sc2;
+
+    for (sc1 = cs; *sc1 != '\0'; ++sc1) {
+        for (sc2 = ct; *sc2 != '\0'; ++sc2) {
+            if (*sc1 == *sc2)
+                return (char *)sc1;
+        }
+    }
+    return 0;
+}
+
+/**
+ * @brief Split a string into tokens, updates s to point after the token, ready for the next call.
+ * 
+ * @param s The string to be searched
+ * @param ct The characters to search for
+ * @return char* 
+ */
+char *strsep(char **s, const char *ct)
+{
+    char *sbegin = *s;
+    char *end;
+
+    if (sbegin == 0)
+        return 0;
+
+    end = strpbrk(sbegin, ct);
+    if (end)
+        *end++ = '\0';
+    *s = end;
+    return sbegin;
+}
--- src/string.h
+++ src/string.h
@@ -1,2 +1,4 @@
 int strcmp(const char *, const char *);
 int strncmp(const char *, const char *, unsigned int);
+char *strpbrk(const char *, const char *);
+char *strsep(char **, const char *);
