f94454c2bc46d9c7edf5eb1ac9a0721f29d5d749
Roger <nyps6516@gmail.com>
2022-03-16 01:09:47 +0800

    Modify the register using for store x0.
    The original we use x20 to store the value of x0 to avoid the dtb
    address be overwritten.
    But x20 will be used in bootloader, so we use x12.

--- src/bootloader/bootloader.c
+++ src/bootloader/bootloader.c
@@ -4,7 +4,7 @@ int boot()
 {
     char *kernel_addr = (char *)0x80000;
     asm volatile (
-        "mov x20, x0;"
+        "mov x12, x0;"
     );
     uart_init();
     // uart_puts("Waiting for kernel image...\n");
@@ -26,7 +26,7 @@ int boot()
 
     asm volatile (
         // we must force an absolute address to branch to
-        "mov x0, x20;"
+        "mov x0, x12;"
         "mov x30, 0x80000; ret" // X30 is used as the Link Register and can be referred to as LR.
         // "ret x30" // Return from subroutine, branches unconditionally to an address in a register, with a hint that this is a subroutine return.
     );
