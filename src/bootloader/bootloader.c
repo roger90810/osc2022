de90050898349d6649aaeb11246704c6473d3afb
Roger <nyps6516@gmail.com>
2022-03-15 22:48:51 +0800

    Support devicetree dtb parser.
    add dtb cmd to parse and print out the devicetree.

--- src/bootloader/bootloader.c
+++ src/bootloader/bootloader.c
@@ -3,6 +3,9 @@
 int boot()
 {
     char *kernel_addr = (char *)0x80000;
+    asm volatile (
+        "mov x20, x0;"
+    );
     uart_init();
     // uart_puts("Waiting for kernel image...\n");
     
@@ -20,8 +23,10 @@ int boot()
         kernel_addr[i] = uart_getc();   
     }
     // uart_puts("Read kernel done !! \n");
+
     asm volatile (
         // we must force an absolute address to branch to
+        "mov x0, x20;"
         "mov x30, 0x80000; ret" // X30 is used as the Link Register and can be referred to as LR.
         // "ret x30" // Return from subroutine, branches unconditionally to an address in a register, with a hint that this is a subroutine return.
     );
--- src/main.c
+++ src/main.c
@@ -1,7 +1,13 @@
 #include "uart.h"
 #include "shell.h"
+#include "stdint.h"
+#include "dtb.h"
+
+uint64_t DTB_BASE;
 int main()
 {
+    // register uint64_t x0 asm("x0");  // clang does not support register variable??
+    asm volatile("mov %0, x0" : "=r"(DTB_BASE));
     uart_init();
     start_shell();
     return 0;
