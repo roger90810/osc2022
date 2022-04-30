ca12b408799807c671f7af3ff5d8881dd8ef8fe2
roger <jstsai.cs10@nycu.edu.tw>
2022-04-30 16:11:44 +0800

    Move some type define to types.h

---
+++ src/types.h
@@ -0,0 +1,5 @@
+typedef unsigned char      uint8_t;
+typedef unsigned short int uint16_t;
+typedef unsigned int       uint32_t;
+typedef unsigned long      uint64_t;
+typedef _Bool              bool;
