38787e178bd1aaa9630d3ba6eb24ced7b9074691
roger <jctsai.cs10@nycu.edu.tw>
2022-06-14 02:26:36 +0800

    Implement syscall entry to call svc.

---
+++ src/syscall.h
@@ -0,0 +1,21 @@
+#ifndef __SYSCALL_H
+#define __SYSCALL_H
+
+#include "types.h"
+
+#define SYSCALL_getpid         (0)
+#define SYSCALL_UART_READ       (1)
+#define SYSCALL_UART_WRITE      (2)
+#define SYSCALL_EXEC            (3)
+#define SYSCALL_FORK            (4)
+#define SYSCALL_EXIT            (5)
+
+
+extern uint64_t syscall_getpid();
+extern uint32_t syscall_uart_read(char buf[], uint32_t size);
+extern uint32_t syscall_uart_write(const char buf[], uint32_t size);
+extern int syscall_exec(void(*func)());
+extern int syscall_fork();
+extern void syscall_exit();
+
+#endif 
